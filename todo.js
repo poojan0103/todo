8ed725d61551d164dd491d949df5d5ef51c4afc1
poojan0103 <107676798+poojan0103@users.noreply.github.com>
2023-02-08 18:18:44 +0530

    to-do

--- todo.js
+++ todo.js
@@ -1,165 +1,193 @@
-function validForm() {
-    var title = document.getElementById('title').value;
-    var descriptiopn = document.getElementById('description').value;
-    var sdate = document.getElementById('sdate').value;
-    var edate = document.getElementById('edate').value;
-    var status = document.getElementById('status').value;
+validateForm = () => {
+    let title = document.getElementById("title");
+    let description = document.getElementById("description");
+    let sdate = document.getElementById("sdate");
+    let edate = document.getElementById("edate");
+    // let status = document.getElementById("status");
 
-    if (title == "") {
-        alert("Title is Required")
+    let regEx = /^[a-zA-Z][a-zA-Z 0-9]*$/;
+    if (title.value == "") {
+        alert("*Name Must not be empty");
         return false;
-    }
-    else if (descriptiopn == "") {
-        alert("description is Required")
+    } else if (!regEx.test(title.value)) {
+        alert("*Invalid Name(Must start with alphabet)");
         return false;
-    }
-    else if (sdate == "") {
-        alert("Start Date Is Required")
+    } else if (description.value == "") {
+        alert("*descriptionription must not be empty");
         return false;
-    }
-    else if (edate == "") {
-        alert("End Date is Required")
+    } else if (sdate.value == "") {
+        alert("*Start date must not be empty");
         return false;
-    }
-    else if (status == "") {
-        alert("Status is Require")
+    } else if (edate.value == "") {
+        alert("*End date must not be empty");
         return false;
-    }else{
-        alert("Data is Added")
+    } else if (edate.value < sdate.value) {
+        alert("*End date cant be before Start date");
+        return false;
+    } else {
+        alert("Your task has been Added");
         return true;
     }
-    
-    
-    
-    
-
-
-}
-function showData() {
-    var todolist;
-    if (localStorage.getItem("todolist") == null) {
-        todolist = [];
-
+};
+showList = () => {
+    let list;
+    if (localStorage.getItem("list") == null) {
+        list = [];
     } else {
-        todolist = JSON.parse(localStorage.getItem("todolist"))
+        list = JSON.parse(localStorage.getItem("list"));
     }
 
-    var html = "";
+    for (let i = 0; i < list.length; i++) {
+        list[i].add = 1;
+        list[i].edit = 0;
+    }
 
-    todolist.forEach(function (element, index) {
+    localStorage.setItem("list", JSON.stringify(list));
+    list = JSON.parse(localStorage.getItem("list"));
+
+    let html = "";
+    list.forEach(function (element, index) {
         html += "<tr>";
-
-
-        
-        html += "<td>" + element.title + "</td>"
-        html += "<td>" + element.description + "</td>"
-        html += "<td>" + element.status + "</td>"
-        html += "<td>" + element.sdate + "</td>"
-        html += "<td>" + element.edate + "</td>"
-        html += '<td><button  id="delete" onclick="deleteData(' + index + ')" class="btn btn-danger">Delete</button><button  onclick="updateData(' + index + ')" class="btn btn-primary">Edit</button></td>';
-
-        html += "</tr>"
+        html += "<td>" + (index + 1) + "</td>";
+        html += "<td>" + element.title + "</td>";
+        html += "<td>" + element.description + "</td>";
+        html += "<td>" + element.sdate + "</td>";
+        html += "<td>" + element.edate + "</td>";
+        html += "<td>" + element.status + "</td>";
+        html +=
+            '<td><button id="edit" onclick="editId(' +
+            index +
+            ')" class="btn btn-primary">Edit</button><button id="delate" onclick="deleteList(' +
+            index +
+            ')" type="button" class="btn btn-danger">Delete</button></td>';
+        html += "</tr>";
     });
     document.querySelector("#todotable tbody").innerHTML = html;
+};
 
-}
-document.onload = showData();
+// document.onload() = showList();
 
-function AddData() {
-    if (validForm() == true) {
-        var title = document.getElementById('title').value;
-        var description = document.getElementById('description').value;
-        var sdate = document.getElementById('sdate').value;
-        var edate = document.getElementById('edate').value;
-        var status = document.getElementById('status').value;
+addData = () => {
+    if (validateForm() == true) {
+        let title = document.getElementById("title").value;
+        let description = document.getElementById("description").value;
+        let sdate = document.getElementById("sdate").value;
+        let edate = document.getElementById("edate").value;
+        let status = document.getElementById("status").value;
+        //window.location.href("list.html");
 
-        var todolist;
-        if (localStorage.getItem("todolist") == null) {
-            todolist = [];
-
+        let list;
+        if (localStorage.getItem("list") == null) {
+            list = [];
         } else {
-            todolist = JSON.parse(localStorage.getItem("todolist"))
+            list = JSON.parse(localStorage.getItem("list"));
         }
 
-
-        todolist.push({
+        list.push({
             title: title,
             description: description,
-            status: status,
             sdate: sdate,
             edate: edate,
+            status: status,
+            add: 1,
+            edit: 0
         });
 
-        localStorage.setItem("todolist", JSON.stringify(todolist));
-        showData();
+        localStorage.setItem("list", JSON.stringify(list));
+        showList();
 
         document.getElementById("title").value = "";
         document.getElementById("description").value = "";
-        document.getElementById("status").value = "";
         document.getElementById("sdate").value = "";
         document.getElementById("edate").value = "";
-
-
-
+        document.getElementById("status").value = "";
     }
+};
+
+deleteList = (index) => {
+    let result = confirm("Are You sure to Delate this Item");
+    if (result) {
+        if (localStorage.getItem("list") == null) {
+            list = [];
+        } else {
+            list = JSON.parse(localStorage.getItem("list"));
+        }
+        list.splice(index, 1);
+        localStorage.setItem("list", JSON.stringify(list));
+        showList();
+        return true;
+    } else {
+        return false;
+    }
+};
+
+editId = (index) => {
+    console.log(index);
+    list = JSON.parse(localStorage.getItem("list"));
+
+    list[index].add = 0;
+    list[index].edit = 1;
+
+    localStorage.setItem("list", JSON.stringify(list));
+
+    window.location.assign("to-do.html");
+}
+editList = (index) => {
+    document.getElementById("addbtn").style.display = "none";
+    document.getElementById("update").style.display = "block";
+    document.getElementById("cancel").style.display = "block";
+    
+    if (localStorage.getItem("list") == null) {
+        list = [];
+    } else {
+        list = JSON.parse(localStorage.getItem("list"));
+    }
+
+    document.getElementById("title").value = list[index].title;
+    document.getElementById("description").value = list[index].description;
+    document.getElementById("sdate").value = list[index].sdate;
+    document.getElementById("edate").value = list[index].edate;
+    document.getElementById("status").value = list[index].status;
+
+    document.querySelector("#update").onclick = function () {
+        if (validateForm() == true) {
+            list[index].title = document.getElementById("title").value;
+            list[index].description = document.getElementById("description").value;
+            list[index].sdate = document.getElementById("sdate").value;
+            list[index].edate = document.getElementById("edate").value;
+            list[index].status = document.getElementById("status").value;
+        }
+
+        list[index].add = 1;
+        list[index].edit = 0;
+
+        localStorage.setItem("list", JSON.stringify(list));
+
+        showList();
+        document.getElementById("title").value = " ";
+        document.getElementById("description").value = " ";
+        document.getElementById("sdate").value = " ";
+        document.getElementById("edate").value = " ";
+        document.getElementById("status").value = " ";
+    };
+};
+
+routePage = () => {
+    window.location.assign("list.html");
 }
 
- function deleteData(index) {
-    var result = confirm("Are you sure to delete?")
-    if(result){
-    var todolist;
-    if (localStorage.getItem("todolist") == null) {
-        todolist = [];
+check = () => {
+    list = JSON.parse(localStorage.getItem("list"));
+    //console.log(list);
 
-    } else {
-        todolist = JSON.parse(localStorage.getItem("todolist"))
-    }
-
-    todolist.splice(index, 1);
-    localStorage.setItem("todolist", JSON.stringify(todolist));
-    showData();
-    return true
-    }else{
-    return false;
-    }
-} 
-
-
-function updateData(index){
-    document.getElementById("add").style.display = "none";
-    document.getElementById("update").style.display = "block";
-    if (localStorage.getItem("todolist") == null) {
-        todolist = [];
-
-    } else {
-        todolist = JSON.parse(localStorage.getItem("todolist"))
-    }
-    document.getElementById("title").value = todolist[index].title;
-    document.getElementById("description").value = todolist[index].description;
-    document.getElementById("status").value = todolist[index].status;
-    document.getElementById("sdate").value = todolist[index].sdate;
-    document.getElementById("edate").value = todolist[index].edate;
-   
-    document.querySelector("#update").onclick = function(){
-        if(validForm() == true){
-            todolist[index].title = document.getElementById("title").value;
-            todolist[index].description = document.getElementById("description").value;
-            todolist[index].status = document.getElementById("status").value;
-            todolist[index].sdate = document.getElementById("sdate").value;
-            todolist[index].edate = document.getElementById("edate").value;
-
-            localStorage.setItem("todolist",JSON.stringify(todolist));
-
-            showData();
-
-            document.getElementById("title").value = "";
-            document.getElementById("description").value = "";
-            document.getElementById("status").value = "";
-            document.getElementById("sdate").value = "";
-            document.getElementById("edate").value = "";
-            
-            
-
+    let indexNumber = 0;
+    for (let i = 0; i < list.length; i++) {
+        if (list[i].add == 0 && list[i].edit == 1) {
+            indexNumber = i;
+            //console.log(indexNumber);
+            editList(indexNumber);
+            break;
         }
+
     }
 }
