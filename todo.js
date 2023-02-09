8adf5270aa1b96417a467723ab4fc025ce7966d4
poojan0103 <107676798+poojan0103@users.noreply.github.com>
2023-02-09 12:47:17 +0530

    updated

--- todo.js
+++ todo.js
@@ -1,33 +1,30 @@
 validateForm = () => {
     let title = document.getElementById("title");
-    let description = document.getElementById("description");
     let sdate = document.getElementById("sdate");
     let edate = document.getElementById("edate");
-    // let status = document.getElementById("status");
 
     let regEx = /^[a-zA-Z][a-zA-Z 0-9]*$/;
     if (title.value == "") {
-        alert("*Name Must not be empty");
+        document.getElementById('emptyName').style.display = "block";
         return false;
     } else if (!regEx.test(title.value)) {
-        alert("*Invalid Name(Must start with alphabet)");
-        return false;
-    } else if (description.value == "") {
-        alert("*descriptionription must not be empty");
+        document.getElementById('invalidName').style.display = "block";
         return false;
     } else if (sdate.value == "") {
-        alert("*Start date must not be empty");
+        document.getElementById('emptySdate').style.display = "block";
         return false;
     } else if (edate.value == "") {
-        alert("*End date must not be empty");
+        document.getElementById('emptyEdate').style.display = "block";
         return false;
     } else if (edate.value < sdate.value) {
-        alert("*End date cant be before Start date");
+        document.getElementById('invalidDate').style.display = "block";
         return false;
     } else {
-        alert("Your task has been Added");
+        // alert("Your task has been Added");
+        document.getElementById('addAlert').style.display = "block";
         return true;
     }
+
 };
 showList = () => {
     let list;
@@ -59,13 +56,11 @@ showList = () => {
             index +
             ')" class="btn btn-primary">Edit</button><button id="delate" onclick="deleteList(' +
             index +
-            ')" type="button" class="btn btn-danger">Delete</button></td>';
+            ')" type="button" class="btn btn-danger" >Delete</button></td>';
         html += "</tr>";
     });
     document.querySelector("#todotable tbody").innerHTML = html;
 };
-
-// document.onload() = showList();
 
 addData = () => {
     if (validateForm() == true) {
@@ -74,7 +69,6 @@ addData = () => {
         let sdate = document.getElementById("sdate").value;
         let edate = document.getElementById("edate").value;
         let status = document.getElementById("status").value;
-        //window.location.href("list.html");
 
         let list;
         if (localStorage.getItem("list") == null) {
@@ -94,18 +88,20 @@ addData = () => {
         });
 
         localStorage.setItem("list", JSON.stringify(list));
-        showList();
 
         document.getElementById("title").value = "";
         document.getElementById("description").value = "";
         document.getElementById("sdate").value = "";
         document.getElementById("edate").value = "";
         document.getElementById("status").value = "";
+
+        showList();
     }
 };
 
 deleteList = (index) => {
-    let result = confirm("Are You sure to Delate this Item");
+    let result = confirm("Are You sure to Delete this Item");
+    // console.log(result);
     if (result) {
         if (localStorage.getItem("list") == null) {
             list = [];
@@ -115,6 +111,8 @@ deleteList = (index) => {
         list.splice(index, 1);
         localStorage.setItem("list", JSON.stringify(list));
         showList();
+        
+        document.getElementById("delateAlert").style.display = "block";
         return true;
     } else {
         return false;
@@ -134,9 +132,9 @@ editId = (index) => {
 }
 editList = (index) => {
     document.getElementById("addbtn").style.display = "none";
-    document.getElementById("update").style.display = "block";
-    document.getElementById("cancel").style.display = "block";
-    
+    document.getElementById("update").style.display = "inline-block";
+    document.getElementById("reset").style.display = "none";
+
     if (localStorage.getItem("list") == null) {
         list = [];
     } else {
@@ -150,25 +148,29 @@ editList = (index) => {
     document.getElementById("status").value = list[index].status;
 
     document.querySelector("#update").onclick = function () {
-        if (validateForm() == true) {
-            list[index].title = document.getElementById("title").value;
-            list[index].description = document.getElementById("description").value;
-            list[index].sdate = document.getElementById("sdate").value;
-            list[index].edate = document.getElementById("edate").value;
-            list[index].status = document.getElementById("status").value;
-        }
+        // if (validateForm() == true) {
+        list[index].title = document.getElementById("title").value;
+        list[index].description = document.getElementById("description").value;
+        list[index].sdate = document.getElementById("sdate").value;
+        list[index].edate = document.getElementById("edate").value;
+        list[index].status = document.getElementById("status").value;
+        // }
 
         list[index].add = 1;
         list[index].edit = 0;
 
         localStorage.setItem("list", JSON.stringify(list));
 
-        showList();
+        // console.log("done")
         document.getElementById("title").value = " ";
         document.getElementById("description").value = " ";
         document.getElementById("sdate").value = " ";
         document.getElementById("edate").value = " ";
         document.getElementById("status").value = " ";
+        // alert("Task has been Updated");
+        document.getElementById("updateAlert").style.display = "block";
+        showList();
+        // console.log("done")
     };
 };
 
@@ -178,16 +180,29 @@ routePage = () => {
 
 check = () => {
     list = JSON.parse(localStorage.getItem("list"));
-    //console.log(list);
 
     let indexNumber = 0;
     for (let i = 0; i < list.length; i++) {
         if (list[i].add == 0 && list[i].edit == 1) {
             indexNumber = i;
-            //console.log(indexNumber);
+            console.log(indexNumber);
             editList(indexNumber);
             break;
         }
 
     }
+}
+
+clearVelidation = () => {
+    document.getElementById("emptyName").style.display = "none";
+    document.getElementById("invalidName").style.display = "none";
+    document.getElementById("emptySdate").style.display = "none";
+    document.getElementById("emptyEdate").style.display = "none";
+    document.getElementById("invalidDate").style.display = "none";
+    document.getElementById('addAlert').style.display = "none";
+    document.getElementById("updateAlert").style.display = "none";
+    document.getAnimations("delateAlert").style.display = "none";
+
+
+
 }
